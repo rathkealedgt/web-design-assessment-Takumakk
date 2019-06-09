eb1592bf526138f0cff6d466ef07a839dbe72cfe
Takumakk <50968830+Takumakk@users.noreply.github.com>
2019-06-09 18:27:01 +0900

    add contact page
    Added contact page

---
+++ AS91880/Contact us.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Contact us.aspx.vb" Inherits="AS91880.Contact_us" %>
+
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="review">
+    <meta name="keywords" content="Japanese food, sushi, Japanese restaurant, food">
+    <meta name="author" content="Takuma Kaneko">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Japanese Restaurant Review</title>
+
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="All">
+
+        <div class="in nav">
+            <h1>
+                <a href="index.html">Home</a> |
+                <a href="about.html">About</a> |
+                <a href="Contact us.aspx">Contact us</a>
+            </h1>
+        </div> <!--navigation-->
+
+        <div class="in logo">
+
+        </div> <!--Logo picture-->
+
+        <div class="in aspmain">
+            <form id="form1" runat="server"> 
+
+                <asp:Label ID="Label5" runat="server" Text="Contact us"></asp:Label><br />
+
+                <!--Name-->
+                <asp:Label ID="Label1" runat="server" Text="Name:"></asp:Label><br />
+                <asp:textbox ID="txtname" runat="server" width="180px"></asp:textbox><br />
+
+                <!--email-->
+                <asp:Label ID="Label12" runat="server" Text="E-mail:"></asp:Label><br />
+                <asp:textBox ID="txtmail" runat="server" width="180px"></asp:textBox><br /> 
+
+                <!--restaurant-->
+                <asp:Label ID="Label3" runat="server" Text="Restaurant:"></asp:Label><br />
+                <asp:textBox ID="txtres" runat="server" width="180px"></asp:textBox><br />
+
+                <!--massage-->
+                <asp:Label ID="Label4" runat="server" Text="Massage:"></asp:Label><br />
+                <asp:textBox ID="textmass" runat="server" height="80px" width="180px"></asp:textBox><br />
+
+                <!--submit-->
+                <asp:button ID="Label6" runat="server" Text="Submit"></asp:button><br />
+           
+            </form>
+        </div> <!--Main structure-->
+
+            <div class="in side">
+               <div class="Store">
+                  <h2>My Profile</h2>
+                </div>
+                <p><img src="../image/resize logo.png" alt="logo" width="450" />
+                
+                <div class="sidetext">
+                   Name:<b>181102</b><br>
+                   I'm Japanese international student.<br>
+                   My birth is 15/1/2003<br>
+                </div>
+            </div>
+                
+                
+        <div class="in footer">
+            <div class="text">
+            <small>Made by Takuma Kaneko(c)2019</small>
+            </div>
+        </div> <!--Footer-->
+
+       
+    </div> <!--All-->
+</body>
+</html>
---
+++ AS91880/Contact us.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <自動生成>
+'     このコードはツールによって生成されました。
+'
+'     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+'     コードが再生成されるときに損失したりします。 
+' </自動生成>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Contact_us
+    
+    '''<summary>
+    '''form1 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+    
+    '''<summary>
+    '''Label5 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label5 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Label1 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''txtname コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents txtname As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Label12 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label12 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''txtmail コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents txtmail As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Label3 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''txtres コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents txtres As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Label4 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label4 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''textmass コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents textmass As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Label6 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Label6 As Global.System.Web.UI.WebControls.Button
+End Class
